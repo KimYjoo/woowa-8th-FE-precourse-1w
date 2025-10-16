e8ff9f90dcbbb8919ab79197e6ccea5401ad9142
Yjoo0726 <pas4976@gmail.com>
2025-10-16 15:15:40 +0900

    feat: 입력 문자열에 숫자가 포함됐는지 검증

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
+    // 사용자 입력
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    // 문자열에 숫자가 포함됐는지 검증
+    const hasNumberPattern = new RegExp('\\d');
+    if( !hasNumberPattern.test(input) ) return;
+    
+
+
   }
 }
 

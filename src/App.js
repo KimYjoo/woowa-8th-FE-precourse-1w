ef6063a730d677387fa4fe4f79118ee307e7a955
KimYjoo <pas4976@gmail.com>
2025-10-18 21:52:05 +0900

    refactor: 변수 이름 변경

--- src/App.js
+++ src/App.js
@@ -4,38 +4,36 @@ class App {
     
 
     try{
-      const spliter = new Set([",", ":"]);
+      const delimiterSet = new Set([",", ":"]);
       // 사용자 입력
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const userInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
       // 입력이 공백인지 검증
-      if( !input ) throw new Error('[ERROR]');
+      if( !userInput ) throw new Error('[ERROR]');
       
       // 문자열에 숫자가 포함되었는지 검증
-      const hasNumberPattern = new RegExp('\\d');
+      const regexCheckNumber = new RegExp('\\d');
       // 커스텀 구분자와 일반 문자열을 추출
-      const getCustomPattern = new RegExp('^(?:(?:\/\/)(.*)?(?:\\\\n))?(.*)?');
-      const arrs = getCustomPattern.exec(input);
-      const customSpliters = arrs[1];
-      const normalString = arrs[2];
-
+      const regexParsingString = new RegExp('^(?:(?:\/\/)(.*)?(?:\\\\n))?(.*)?');
+      const [_, customDelimiters, targetString] = regexParsingString.exec(userInput);
       // 커스텀 구분자 정의부가 추출되었는지 확인
-      if( customSpliters ) {
+      if( customDelimiters ) {
         // 커스텀 구분자에 숫자가 있는지 확인
-        if ( hasNumberPattern.test(customSpliters) ) throw new Error('[ERROR]');
+        if ( regexCheckNumber.test(customDelimiters) ) throw new Error('[ERROR]');
         // 커스텀 구분자를 전체 구분자 집합에 추가
-        [...customSpliters].forEach((v) => spliter.add(v));
+        [...customDelimiters].forEach((v) => delimiterSet.add(v));
       }
       
       // 일반 문자열부가 공백인지 검증
-      if( !normalString ) throw new Error('[ERROR]');
+      if( !targetString ) throw new Error('[ERROR]');
       // 일반 문자열부를 등록된 구분자로 분리
-      const useSpliterPattern = new RegExp(`[${Array.from(spliter).join('')}]`);
-      const regexOutsideDelimiter = new RegExp(`[^${Array.from(spliter).join('')}\\d]`);
-      const numbers = normalString.split(useSpliterPattern);
-      if(regexOutsideDelimiter.test(normalString)) throw new Error('[ERROR]');
+      const regexMatchDelimiter = new RegExp(`[${Array.from(delimiterSet).join('')}]`);
+      const regexOutsideDelimiter = new RegExp(`[^${Array.from(delimiterSet).join('')}\\d]`);
+      const delimitedStringArray = targetString.split(regexMatchDelimiter);
+      
+      if(regexOutsideDelimiter.test(targetString)) throw new Error('[ERROR]');
       // 배열의 요소들을 숫자로 변환하여 모두 더한 값을 저장
-      const sum = numbers.reduce((acc, cur) => {
+      const sumResult = delimitedStringArray.reduce((acc, cur) => {
         const num = Number(cur);
         // 배열 요소가 숫자로 변환가능한지 검증
         if(Number.isFinite(num)){
@@ -46,7 +44,7 @@ class App {
         }
       }, 0);
 
-      Console.print(`결과 : ${sum}`);
+      Console.print(`결과 : ${sumResult}`);
     }
     catch(e) {
       Console.print(e);

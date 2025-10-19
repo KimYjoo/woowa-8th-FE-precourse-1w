7e82f5bcabc02ee411149277a2fa26948d0e7ebe
KimYjoo <pas4976@gmail.com>
2025-10-19 19:08:07 +0900

    refactor: 중첩 if 제거

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
     
 
     try{
-      const delimiterSet = new Set([",", ":"]);
+      const baseDelimiters = new Set([",", ":"]);
       // 사용자 입력
       const userInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
@@ -13,43 +13,41 @@ class App {
       
       // 문자열에 숫자가 포함되었는지 검증
       const regexCheckNumber = new RegExp('\\d');
-      // 커스텀 구분자와 일반 문자열을 추출
-      const regexParsingString = new RegExp('^(?:(?:\/\/)(.*)?(?:\\\\n))?(.*)?');
       // 커스텀 구분자 정의부가 있는지 검증
       const regexCheckCustomRegister = new RegExp('^(\/\/)(?:.*)?(\\\\n)');
-      const [_, customDelimiters, targetString] = regexParsingString.exec(userInput);
-      Console.print(customDelimiters)
-      // 커스텀 구분자 정의부가 추출되었는지 확인
-      if( customDelimiters ) {
-        // 커스텀 구분자에 숫자가 있는지 확인
-        if ( regexCheckNumber.test(customDelimiters) ) throw new Error('[ERROR]');
-        // 커스텀 구분자를 전체 구분자 집합에 추가
-        [...customDelimiters].forEach((v) => delimiterSet.add(v));
-      }
-      // 커스텀 구분자 정의부가 있지만 내부가 비었을 경우 검증
-      else if(regexCheckCustomRegister.test(userInput)){
-        throw new Error('[ERROR]');
-      }
       
+      // 커스텀 구분자와 일반 문자열을 추출
+      const regexParseCustomDelimiter = new RegExp('^(?:(?:\/\/)(.*)?(?:\\\\n))?(?:.*)?');
+      const regexParseTargetString = new RegExp('^(?:(?:\/\/)(?:.*)?(?:\\\\n))?(.*)?');
+      const [, customDelimiters] = regexParseCustomDelimiter.exec(userInput);
+      const [, targetString] = regexParseTargetString.exec(userInput);
+
+      const getCustomDelimiter = (set, str) => {
+        // 문자열에 커스텀 구분자 정의부가 없다면 바로 반환
+        if(!regexCheckCustomRegister.test(str)) return set;
+        // 정의부가 있지만 내용이 없을 때 에러 
+        if(!customDelimiters) throw new Error('[ERROR]');
+        // 숫자가 구분자로 사용됐을 때 에러
+        if(regexCheckNumber.test(customDelimiters)) throw new Error('[ERROR]');
+        // 커스텀 구분자를 전체 구분자 집합에 추가
+        [...customDelimiters].forEach((v) => set.add(v));
+        return set;
+      }
+      const allOfDelimiter = getCustomDelimiter(baseDelimiters, userInput);
+
       // 일반 문자열부가 공백인지 검증
       if( !targetString ) throw new Error('[ERROR]');
       // 일반 문자열부를 등록된 구분자로 분리
-      const regexMatchDelimiter = new RegExp(`[${Array.from(delimiterSet).join('')}]`);
-      const regexOutsideDelimiter = new RegExp(`[^${Array.from(delimiterSet).join('')}\\d]`);
+      const regexMatchDelimiter = new RegExp(`[${Array.from(allOfDelimiter).join('')}]`);
+      const regexOutsideDelimiter = new RegExp(`[^${Array.from(allOfDelimiter).join('')}\\d]`);
       const delimitedStringArray = targetString.split(regexMatchDelimiter);
 
+      // 타겟 문자열 부에 등록된 구분자와 숫자 외에 다른 문자가 있을 경우 에러
       if(regexOutsideDelimiter.test(targetString)) throw new Error('[ERROR]');
+      // 타겟 문자열 부에 숫자가 없을 경우 에러
+      if(!regexCheckNumber.test(targetString)) throw new Error('[ERROR]');
       // 배열의 요소들을 숫자로 변환하여 모두 더한 값을 저장
-      const sumResult = delimitedStringArray.reduce((acc, cur) => {
-        const num = Number(cur);
-        // 배열 요소가 숫자로 변환가능한지 검증
-        if(Number.isFinite(num)){
-          return acc + num;
-        }
-        else{
-          throw new Error('[ERROR]');
-        }
-      }, 0);
+      const sumResult = delimitedStringArray.reduce((acc, cur) => (acc + Number(cur)), 0);
 
       //결과 출력
       Console.print(`결과 : ${sumResult}`);
